9ada21cd71525668d0d1ee7452ea1f871729b8bf
liu <87763097+rlxbz@users.noreply.github.com>
2025-11-07 16:43:39 +0800

    Delete 新建文件夹 directory

--- 新建文件夹/src/apis/notice.js
+++
@@ -1,9 +0,0 @@
-import request from '@/utils/http'
-
-export const getNoticeListAPI = (params) => {
-  return request.get('/notice/list', { params })
-}
-
-export const getNoticeDetailAPI = (id) => {
-  return request.get(`/notice/${id}`)
-}
