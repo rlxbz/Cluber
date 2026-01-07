1a8853d16a2762967550025298d6b29c9409b6dd
liu <87763097+rlxbz@users.noreply.github.com>
2026-01-07 21:02:15 +0800

    Delete 新建文件夹 directory

--- 新建文件夹/src/apis/push.js
+++
@@ -1,9 +0,0 @@
-import request from '@/utils/http'
-
-export const getPushListAPI = (params) => {
-  return request.get('/push/list', { params })
-}
-
-export const getPushDetailAPI = (id) => {
-  return request.get(`/push/${id}`)
-}
